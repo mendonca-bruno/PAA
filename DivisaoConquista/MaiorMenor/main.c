6b469c3238482a2b9cd5086a5af9dcffbe543a9a
mendonca-bruno <37663078+mendonca-bruno@users.noreply.github.com>
2019-06-29 05:30:35 -0300

    div_conquista_working

--- DivisaoConquista/MaiorMenor/main.c
+++ DivisaoConquista/MaiorMenor/main.c
@@ -1,28 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int *calculaMaiorMenor(int *s, int n){
+int *calculaMaiorMenor(int *vetor, int n, int inicio, int fim)
+{
     int *temp;
     if(n==2){
+
         temp = (int *) malloc (2 * sizeof(int));
-        if(s[0]>s[1]){
-            temp[0] = s[0];
-            temp[1] = s[1];
+        if(vetor[inicio]>vetor[fim]){
+            temp[0] = vetor[inicio];
+            temp[1] = vetor[fim];
         }else{
-            temp[0] = s[1];
-            temp[1] = s[0];
+            temp[0] = vetor[fim];
+            temp[1] = vetor[inicio];
         }
     }else{
-        temp = calculaMaiorMenor(s,n-1);
-        if(s[n-1]>temp[0]) temp[0] = s[n-1];
-        if(s[n-1]<temp[1]) temp[1] = s[n-1];
+        temp = (int *) malloc (2 * sizeof(int));
+        int meio = (inicio+fim)/2;
+        int*esq,*dir;
+        int qtd;
+    
+        if(n%2==0){
+            qtd = ((fim-inicio)+1)/2;
+            esq = calculaMaiorMenor(vetor, qtd,inicio,meio);
+            dir = calculaMaiorMenor(vetor, qtd, meio+1,fim);
+        }else{
+            qtd = (((fim-inicio)+1)/2)+1;
+            esq = calculaMaiorMenor(vetor, qtd,inicio,meio);
+            dir = calculaMaiorMenor(vetor, qtd, meio,fim);
+        }
+                
+        if(esq[0]>dir[0]) temp[0] = esq[0];
+        else temp[0] = dir[0];
+        if(esq[1]<dir[1]) temp[1] = esq[1];
+        else temp[1] = dir[1];
+       free(esq);
+       free(dir);  
+        
     }
+    
     return temp;
 }
 
+
 int main(){
-    int s[4] = {1,2,3,4};
-    int *res = calculaMaiorMenor(s,4);
+    int out;
+    int inicio = 0;
+    int qtd;
+    int min,max,aleatorio;
+    printf("Digite o tamanho do vetor:");
+    scanf("%d", &qtd);
+    printf("\n");
+    int vetor[qtd];
+    for(int i=0; i<qtd; i++){
+        printf("Digite um numero: ");
+        scanf("%d", &vetor[i]);
+    }
+    int fim = qtd-1;
+    int *res = calculaMaiorMenor(vetor,qtd,inicio,fim);
     printf("Maior(%d)\nMenor(%d)", res[0],res[1]);
+    scanf("\n%d", &out);
     return 0;
 }
